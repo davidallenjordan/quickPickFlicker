1880a4e34ea9d80ee7d26f555a13730c7ed6ed98
Krupali Thakkar <krupalitp@outlook.com>
2020-08-15 17:23:17 -0400

    added movie details component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
-import axios from 'axios';
+import MovieDetails from './components/MovieDetails';
 
 class App extends Component {
   constructor() {
@@ -11,49 +11,22 @@ class App extends Component {
     }
   }
 
+  //attaches firebase to the app
   componentDidMount() {
     const dbRef = firebase.database().ref();
     dbRef.on('value', (snapshot) => {
       const newState = [];
-      const data = snapshot.val();
-      console.log(data);
-
       this.setState({
         userMovieList: newState
       })
     })
-
-    // API Call
-    axios({
-      url: 'https://api.themoviedb.org/3/discover/movie/',
-      params: {
-        api_key: '2e86861dd566ea2f61741d264de6590a',
-        language: 'en-US',
-        sort_by: 'popularity.desc',
-        include_adult: 'false',
-        include_video: 'false',
-        page: 1,
-      }
-    })
-    .then( (res) => {
-      console.log(res)
-
-      this.setState({
-        movies: res.data.results
-      })
-
-    })
-
-
-
   }
-
-
 
   render() {
     return (
       <div>
         <h1>Pick Flick</h1>
+        <MovieDetails />
       </div>
     );
   }
