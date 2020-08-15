f467c57cfdc1c76f20f56475776b59623b7b3576
David Allen-Jordan <davidallenjordan@gmail.com>
2020-08-15 12:05:22 -0400

    Paired programming - added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      userMovieList: []
+    }
+  }
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+    dbRef.on('value', (snapshot) => {
+      const newState = [];
+      const data = snapshot.val();
+      console.log(data);
+
+      this.setState({
+        userMovieList: newState
+      })
+    })
+
+    // API Call
+    axios({
+      url: 'https://api.themoviedb.org/3/discover/movie/',
+      params: {
+        api_key: '2e86861dd566ea2f61741d264de6590a',
+        language: 'en-US',
+        sort_by: 'popularity.desc',
+        include_adult: 'false',
+        include_video: 'false',
+        page: 1,
+      }
+    })
+    .then( (res) => {
+      console.log(res)
+
+      this.setState({
+        movies: res.data.results
+      })
+
+    })
+
+
+
+  }
+
+
+
   render() {
     return (
       <div>
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>Developed by Krupali Thakkar, David Allen-Jordan, Clayton Puno, Pik Lin Hoe</p>
+    </footer>
+  )
+}
+export default Footer
